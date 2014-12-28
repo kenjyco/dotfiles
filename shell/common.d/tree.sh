c669ecd0cca39a7c369f86a16bf5a958e652d586
Ken <kenjyco@gmail.com>
2014-12-27 20:23:47 -0600

    Update `shell/common.d/tree.sh`
    The `t` function now accepts an optional second argument to specify the
    level/depth.
    
    There are also multiple `_ignore_tree*` variables, some of which build
    upon each other. These are primarily used in the aliases `tgit`,
    `tdotfiles`, and `thome`.

--- shell/common.d/tree.sh
+++ shell/common.d/tree.sh
@@ -1,16 +1,26 @@
-_ignore_tree_string="-I '.git|.cache|chromium|.dbus|.pki|.wicd'"
+_ignore_tree="-I '.git|*.py[ocd]'"
+_ignore_tree_dotfiles="${_ignore_tree%?}|backup_dotfiles|extra|Vundle.vim'"
+_ignore_tree_home="${_ignore_tree_dotfiles%?}|.cache|chromium|.dbus|.pki|.wicd|.grip|.autoenv*'"
+_ignore_tree_home="${_ignore_tree_home%?}|.plugin_install_dir|.autoenv*'"
 
 t() {
     dirname="$1"
+    level="$2"
     [[ -z "$dirname" ]] && dirname="."
     [[ ! -d "$dirname" ]] && dirname="."
     [[ "$dirname" == "." ]] && dirname=$(pwd)
-    eval "tree -Fa $_ignore_tree_string $dirname | less -FX"
+
+    if [[ "$level" =~ [0-9]+ ]]; then
+        eval "tree -Fa -L $level $_ignore_tree $dirname | less -FX"
+    else
+        eval "tree -Fa $_ignore_tree $dirname | less -FX"
+    fi
 }
 
 alias ta="tree -Fa"
 alias td="tree -Fd"
 alias tad="tree -Fad"
 alias tda="tree -Fda"
-alias tgit="tree -Fa -I '.git' | less -FX"
-alias thome="tree -Fa $_ignore_tree_string $HOME | less -FX"
+alias tgit="tree -Fa $_ignore_tree | less -FX"
+alias tdotfiles="tree -Fa $_ignore_tree_dotfiles $(cat $HOME/.dotfiles_path) | less -FX"
+alias thome="tree -Fa $_ignore_tree_home $HOME | less -FX"
