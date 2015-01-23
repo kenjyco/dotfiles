fbfa9ba44b84c6e68bd6e2f63f29a4e178f0a25b
Ken <kenjyco@gmail.com>
2015-01-22 21:52:47 -0600

    Add `bin/lockscreen` and update `install_system.bash`
    The lockscreen script will lock the screen with xscreensaver (running X
    server) or vlock (using console).
    
    Update the `install_system.bash` script to install vlock and termsaver.
    The termsaver program is more for fun and cannot be used for screen
    locking.

--- install_system.bash
+++ install_system.bash
@@ -26,7 +26,7 @@ echo -e "\nsudo apt-get install..."
 echo -e "\nEssential stuff"
 sudo apt-get install -y vim git zsh tmux curl pmount acpi diffstat colordiff htop tree ranger
 sudo apt-get install -y openssh-server rxvt-unicode-256color feh scrot imagemagick emelfm2
-sudo apt-get install -y gtypist typespeed cmatrix elinks enscript evince-gtk
+sudo apt-get install -y gtypist typespeed cmatrix elinks enscript vlock termsaver evince-gtk
 # sudo apt-get install -y wodim dvd+rw-tools
 
 echo -e "\nPython stuff"
