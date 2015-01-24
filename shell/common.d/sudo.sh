4cc5bf7b3ef49e0e660ee7944dfb6de619e96912
Ken <kenjyco@gmail.com>
2015-01-24 14:00:37 -0500

    Make `newuser` function more robust and add `purgeuser`
    The `newuser` function has the option of creating a new user with sudo
    access. Also, forces creation of a password with `passwd`
    
    The `purgeuser` function removes a user from the system.

--- shell/common.d/sudo.sh
+++ shell/common.d/sudo.sh
@@ -5,8 +5,47 @@ alias partitions2="sudo blkid"
 newuser() {
     username=$1
     [[ -z "$username" ]] && exit 1
-    sudo useradd -m -s $(which zsh) $username
-    sudo usermod -a -G audio,video,plugdev $username
 
+    # Create the user
+    echo -e "\nsudo useradd -m -s $(which zsh) $username" >&2
+    sudo useradd -m -s $(which zsh) $username || exit 1
+
+    # Add the user to groups
+    if [[ "$2" == sudo ]]; then
+        sudo usermod -a -G sudo,audio,video,plugdev,netdev $username
+    else
+        sudo usermod -a -G audio,video,plugdev,netdev $username
+    fi
+
+    # Add an empty zshrc file so zsh doesn't bug you on first login
+    sudo touch /home/$username/.zshrc
+
+    # Set a password for the user
+    echo -e "\nsudo passwd $username" >&2
+    sudo passwd $username
+
+    # Clone dotfiles
+    echo -e "\nsudo -u $username git clone https://github.com/kenjyco/dotfiles /home/$username/.dotfiles" >&2
     sudo -u $username git clone https://github.com/kenjyco/dotfiles /home/$username/.dotfiles
+
+    # Make sure the new users owns all the stuff in their directory
+    sudo chown -R $username:$username /home/$username/
+}
+
+purgeuser() {
+    username=$1
+    [[ -z "$username" ]] && return 1
+
+    # Find out if the user is sure
+    if [[ -n "$BASH_VERSION" ]]; then
+        read -p "are you sure? [y/n] " yn
+    elif [[ -n "$ZSH_VERSION" ]]; then
+        vared -p "are you sure? [y/n] " -c yn
+    fi
+
+    [[ ! "$yn" =~ [yY].* ]] && return 1
+
+    # Actually remove the user and their home directory
+    echo -e "\nsudo deluser --remove-home $username" >&2
+    sudo deluser --remove-home $username
 }
