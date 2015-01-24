2119d08f5a7b9f18045a9be8739852f426aee370
Ken <kenjyco@gmail.com>
2015-01-24 14:03:33 -0500

    Update `lockscreen` function to detach tmux session
    If calling from the console in a tmux session, `tmux detach-client` is
    called and the user is prompted to run the function again.
    
    If `vlock` is called in a tmux pane, only that pane is locked, which is
    dumb.

--- shell/common.d/xscreensaver.sh
+++ shell/common.d/xscreensaver.sh
@@ -7,7 +7,14 @@ startsaver() {
 
 lockscreen() {
     if [[ -z "$DISPLAY" ]]; then
-        vlock
+        if [[ -n "$TMUX" ]]; then
+            echo "Call 'lockscreen' again after tmux detaches..."
+            sleep 2
+            tmux detach-client
+            return
+        else
+            vlock
+        fi
     else
         [[ -z $(pgrep xscreensaver) ]] && /usr/bin/xscreensaver -no-splash &
         xscreensaver-command -lock
