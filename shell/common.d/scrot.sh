ffa18c3a65afe6e7911e7dd649a47e3ef2ed8b1d
Ken <kenjyco@gmail.com>
2015-01-24 21:49:43 -0600

    Don't create a screenshot directory by default

--- shell/common.d/scrot.sh
+++ shell/common.d/scrot.sh
@@ -1,6 +1,5 @@
-[[ ! -d "$HOME/screenshots" ]] && mkdir "$HOME/screenshots"
-
 screenshot() {
+    [[ ! -d "$HOME/screenshots" ]] && mkdir "$HOME/screenshots"
     scrot -s '%Y_%m%d--%H%M_%S--'`hostname`'--$wx$h.png' \
           -e 'mv $f ~/screenshots 2>/dev/null'
 }
