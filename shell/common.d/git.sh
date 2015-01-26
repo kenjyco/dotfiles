75472f5547849ed3bdbeccb599925627c2b1d403
Ken <kenjyco@gmail.com>
2015-01-26 00:39:38 -0600

    Add `repo-remotes` and `repo-status` functions
    The `repo-remotes` function will just grep and "url" lines in the
    `.git/config` files for each git repository in ~.
    
    The `repo-status` function does a `git status -s` on each git repository
    in ~ and outputs which files are modified (if any).

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -9,3 +9,21 @@ repo-list() {
     [[ ! "$level" =~ [0-9]+ ]] && level=3
     find ~ -maxdepth $level -type d -name ".git" | xargs -d \\n dirname 2>/dev/null | sort
 }
+
+repo-remotes() {
+    for repo in $(repo-list | xargs -d \\n); do
+        echo -e "\n$repo"
+        grep url $repo/.git/config
+    done
+}
+
+_repo-status() {
+    oldpwd=$(pwd)
+    for repo in $(repo-list | xargs -d \\n); do
+        cd $repo
+        output=$(git status -s)
+        [[ ! -z "$output" ]] && echo -e "\n===============\n$(pwd)\n$output"
+    done
+    cd "$oldpwd"
+}
+alias repo-status="_repo-status | less -FX"
