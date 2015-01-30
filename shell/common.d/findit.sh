11f71e33bac6a0544acc839ea2f47704ff13041b
Ken <kenjyco@gmail.com>
2015-01-29 19:28:12 -0600

    Add more `findit-functions`
    These are all wrappers to the `bin/findit` script
    - `findit-pics`
    - `findit-vids`
    - `findit-audio`
    - `findit-docs`

--- shell/common.d/findit.sh
+++ shell/common.d/findit.sh
@@ -9,3 +9,31 @@ findit-py() {
     [[ ! -z "$dirname" ]] && shift || dirname="."
     findit $dirname --pattern "*.py" $@
 }
+
+findit-pics() {
+    dirname=$1
+    [[ ! -z "$dirname" ]] && shift || dirname="."
+    findit $dirname --complex "\( -iname '*.png' -o -iname '*.jpg' \
+        -o -iname '*.jpeg' -o -iname '*.gif' \)" $@
+}
+
+findit-vids() {
+    dirname=$1
+    [[ ! -z "$dirname" ]] && shift || dirname="."
+    findit $dirname --complex "\( -iname '*.mp4' -o -iname '*.flv' \
+        -o -iname '*.mkv' -o -iname '*.ogv' -o -iname '*.avi' \)" $@
+}
+
+findit-audio() {
+    dirname=$1
+    [[ ! -z "$dirname" ]] && shift || dirname="."
+    findit $dirname --complex "\( -iname '*.mp3' -o -iname '*.flac' \
+        -o -iname '*.ogg' -o -iname '*.wav' \)" $@
+}
+
+findit-docs() {
+    dirname=$1
+    [[ ! -z "$dirname" ]] && shift || dirname="."
+    findit $dirname --complex "\( -iname '*.pdf' -o -iname '*.doc' \
+        -o -iname '*.odt' \)" $@
+}
