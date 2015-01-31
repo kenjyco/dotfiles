cb931bd0733be70d1860aa523da0fb965de4bfcd
Ken <kenjyco@gmail.com>
2015-01-30 21:24:11 -0600

    Add `repos-update-all` function and `branchname` alias
    The `repos-update-all` function will perform a `git pull` on the
    repositories found by `repo-list`. If a dirty repository is on the
    master or production branch, perform a "stash pull pop".
    
    If the repo is dirty and the branch isn't master or production, don't do
    anything.

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -1,4 +1,5 @@
 alias update-submodules="git submodule foreach 'git checkout master && git pull origin master'"
+alias branchname="git rev-parse --abbrev-ref HEAD"
 
 alias glog="git log --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an'"
 alias glog2="glog --date local --name-status"
@@ -27,3 +28,27 @@ _repo-status() {
     cd "$oldpwd"
 }
 alias repo-status="_repo-status | less -FX"
+
+repos-update-all() {
+    eval `ssh-agent -s`
+    ssh-add
+
+    oldpwd=$(pwd)
+    for repo in $(repo-list | xargs -d \\n); do
+        cd $repo
+        echo -e "\n===============\n$(pwd)"
+        gstatus=$(git status -s)
+        if [[ -z "$gstatus" ]]; then
+            echo " - Repository is clean"
+            git pull
+        elif [[ $(git rev-parse --abbrev-ref HEAD) =~ (master|production) ]]; then
+            echo " - Dirty repo on master or production.. gonna do 'stash pull pop'"
+            git stash
+            git pull
+            git stash pop
+        else
+            echo " - Dirty random branch, not updating"
+        fi
+    done
+    cd "$oldpwd"
+}
