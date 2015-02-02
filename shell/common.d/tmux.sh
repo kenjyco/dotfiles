c5708e423025a8c05000b20aadfb79f8dc650ecd
Ken <kenjyco@gmail.com>
2015-02-01 18:08:52 -0600

    Update `Tmux` function to have better default actions
    If no session name is passed to `Tmux`, then it's probably random stuff
    that should go inside a directory for the day's random things.

--- shell/common.d/tmux.sh
+++ shell/common.d/tmux.sh
@@ -1,10 +1,14 @@
-# Shortcut to detach & re-attach to a session that will also create the session
-# if it doesn't exist
+# Detach and re-attach to a specified session.
+#   - if the session doesn't exist, create it
+#   - if no session is specified, use a session named for the current date
+#     (i.e. Feb-01) and start the session from "$HOME/autoday/$session_name/"
 Tmux () {
     session_name=$1
     if [[ -z "$session_name" ]]; then
-        tmux -2
-    else
-        tmux -2 attach-session -t $session_name -d 2>/dev/null || tmux -2 new-session -s $session_name
+        session_name=$(date +%b-%d)
+        session_dir="$HOME/autoday/$session_name"
+        mkdir -p "$session_dir" 2>/dev/null && cd "$session_dir"
     fi
+
+    tmux -2 attach-session -t $session_name -d 2>/dev/null || tmux -2 new-session -s $session_name
 }
