8cde6469f24b566100f4725de5e93dd610f458af
Ken <kenjyco@gmail.com>
2015-04-26 02:29:30 -0500

    Update `repos-update-all` function and `update-submodules` alias
    If a repo is clean and only has untracked files, don't do `git stash
    pop` after `git pull`.
    
    Also, if a repo is local-only (no remote specified), don't attempt to do
    `git pull`.
    
    When updating submodules, use the `--recursive` flag in case the
    submodules have submodules.

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -1,4 +1,4 @@
-alias update-submodules="git submodule foreach 'git checkout master && git pull origin master'"
+alias update-submodules="git submodule foreach --recursive git pull origin master"
 alias branchname="git rev-parse --abbrev-ref HEAD 2>/dev/null"
 
 alias glog="git log --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an'"
@@ -69,15 +69,24 @@ repos-update-all() {
         cd $repo
         echo -e "\n===============\n$(pwd)"
         filestatus=$(git status -s)
+        remote=$(grep "remote \"origin\"" -A 2 .git/config | grep url | perl -pe 's/^\s+url = (.*)$/$1/')
         branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-        if [[ -z "$filestatus" ]]; then
-            echo " - Repository is clean"
+        if [[ -z "$remote" ]]; then
+            echo " - Local-only repo, not updating"
+        elif [[ -z "$filestatus" ]]; then
+            echo " - Repository is clean.. gonna do 'pull'"
             git pull
         elif [[ "$branch" =~ (master|production) ]]; then
-            echo " - Dirty repo on master or production.. gonna do 'stash pull pop'"
-            git stash
-            git pull
-            git stash pop
+            stashstatus=$(git stash)
+            if [[ $stashstatus == "No local changes to save" ]]; then
+                echo " - Repository is clean (w/ untracked files).. gonna do 'pull'"
+                git pull
+            else
+                echo " - Dirty repo on master or production.. gonna do 'stash pull pop'"
+                echo "$stashstatus"
+                git pull
+                git stash pop
+            fi
         else
             echo " - Dirty random branch, not updating"
         fi
