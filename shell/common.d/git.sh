2e85b1fdb344e36d19b137b779f020a5789c693c
Ken <kenjyco@gmail.com>
2015-05-31 16:58:52 -0500

    Add `bin/git/` directory to replace `shell/common.d/git.sh`
    Many of the functions defined in `common.d/git.sh` are being broken up
    into smaller individual scripts. More to come...

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -1,59 +1,28 @@
 alias update-submodules="git submodule foreach --recursive git pull origin master"
-alias branchname="git rev-parse --abbrev-ref HEAD 2>/dev/null"
 
 alias glog="git log --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an'"
 alias glog2="glog --date local --name-status"
-alias glogp="git log -p --full-diff"
 
 gstatus() {
-    oldpwd=$(pwd)
+    _repo_path=$(repo-path)
+    _repo_remote=$(repo-remote)
+    _repo_branch=$(branchname)
+    newcommits=$(unpushed-commits)
 
-    # Determine repository path
-    dirname=$(pwd)
-    while [[ ! -d "$dirname/.git" ]]; do
-        cd ..
-        dirname=$(pwd)
-
-        if [[ $(pwd) == '/' ]]; then
-            cd $oldpwd
-            return 1
-        fi
-    done
-    _repo_path=$(pwd)
-
-    # Determine other relevant info
-    _repo_remote=$(grep "remote \"origin\"" -A 2 .git/config | grep url | perl -pe 's/^\s+url = (.*)$/$1/')
-    _repo_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    newcommits=$(git log --oneline @{u}.. 2>/dev/null)
-
-    cd $oldpwd
     echo -e "$_repo_path -- $_repo_remote -- $_repo_branch"
     pwd
     git status -s
     [[ -n "$newcommits" ]] && echo -e "\nNot pushed\n$newcommits"
 }
 
-repo-list() {
-    level=$1
-    [[ ! "$level" =~ [0-9]+ ]] && level=3
-    find ~ -maxdepth $level -type d -name ".git" | xargs -d \\n dirname 2>/dev/null | sort
-}
-
-repo-remotes() {
-    for repo in $(repo-list | xargs -d \\n); do
-        _repo_remote=$(grep "remote \"origin\"" -A 2 $repo/.git/config | grep url | perl -pe 's/^\s+url = (.*)$/$1/')
-        echo -e "$repo -- $_repo_remote"
-    done
-}
-
 _repo-status() {
     oldpwd=$(pwd)
     echo "Showing repos that have changes"
-    for repo in $(repo-list | xargs -d \\n); do
+    for repo in $(all-repos-list | xargs -d \\n); do
         cd $repo
         filestatus=$(git status -s)
-        newcommits=$(git log --oneline @{u}.. 2>/dev/null)
-        branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+        newcommits=$(unpushed-commits)
+        branch=$(branchname)
         if [[ -n "$filestatus" || -n "$newcommits" ]]; then
             echo -e "\n===============\n$(pwd) -- $branch\n$filestatus"
             [[ -n "$newcommits" ]] && echo -e "\nNot pushed\n$newcommits"
@@ -68,12 +37,13 @@ repos-update-all() {
     sshlazy
 
     oldpwd=$(pwd)
-    for repo in $(repo-list | xargs -d \\n); do
+    for repo in $(all-repos-list | xargs -d \\n); do
         cd $repo
         echo -e "\n===============\n$(pwd)"
         filestatus=$(git status -s)
-        remote=$(grep "remote \"origin\"" -A 2 .git/config | grep url | perl -pe 's/^\s+url = (.*)$/$1/')
-        branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+        # remote=$(grep "remote \"origin\"" -A 2 .git/config | grep url | perl -pe 's/^\s+url = (.*)$/$1/')
+        remote=$(repo-remote)
+        branch=$(branchname)
         if [[ -z "$remote" ]]; then
             echo " - Local-only repo, not updating"
         elif [[ -z "$filestatus" ]]; then
