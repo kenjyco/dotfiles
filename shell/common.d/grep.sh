72b4f6d5592e3b8d25fc3d25b2feaefd567e3f8f
Ken <kenjyco@gmail.com>
2015-08-08 20:47:00 -0500

    Add 'pygrep' function to search some hardcoded paths

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -10,3 +10,15 @@ shgrep() {
     ~/.shell/bash/bash* ~/.shell/zsh/zshrc ~/.shell/common ~/.shell/common.d/* \
     ~/bin/**/* ~/.config/ranger/rc.conf ~/.config/awesome/rc.lua 2>/dev/null
 }
+
+pygrep() {
+    pattern=$1
+    a=$2
+    b=$3
+    [[ -z "$pattern" ]] && exit 1
+    [[ -z "$a" ]] && a=0
+    [[ -z "$b" ]] && b=0
+
+    # FIXME: Shouldn't hardcode these links in
+    grep --color -Hn -P $pattern -A $a -B $b ~/kenjyco/(^env/)#*.py ~/extract_utils/(^env/)#*.py 2>/dev/null
+}
