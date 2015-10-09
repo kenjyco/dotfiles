1bcfe8f3d97abd02bee37bd706888573f346be3d
Ken <kenjyco@gmail.com>
2015-10-09 07:12:09 -0500

    Update getmusic function to dl to /music/new

--- shell/common.d/youtube.sh
+++ shell/common.d/youtube.sh
@@ -1,12 +1,12 @@
 getmusic() {
-    if [[ ! -d /music ]]; then
-        echo "/music directory needs to be created"
-        sudo mkdir -v /music || return 1
-        sudo chmod 777 /music
-        sudo chown -R $(whoami):$(whoami) /music
+    if [[ ! -d /music/new ]]; then
+        echo "/music/new directory needs to be created"
+        sudo mkdir -pv /music/new || return 1
+        sudo chmod 777 /music/new
+        sudo chown -R $(whoami):$(whoami) /music/new
     fi
 
-    cd /music
+    cd /music/new
     youtube $1 --audio --description
     cd -
 }
