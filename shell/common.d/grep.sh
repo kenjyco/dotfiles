f1b4fc7778121a117df4c92ef5aa80e22cb89123
Ken <kenjyco@gmail.com>
2016-04-24 13:58:39 -0500

    Add 'grepit' function and remove 'shgrep' and 'pygrep'

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -1,27 +1,3 @@
-shgrep() {
-    pattern=$1
-    a=$2
-    b=$3
-    [[ -z "$pattern" ]] && exit 1
-    [[ -z "$a" ]] && a=0
-    [[ -z "$b" ]] && b=0
-
-    grep --color -Hn -P $pattern -A $a -B $b \
-    ~/.shell/bash/bash* ~/.shell/zsh/zshrc ~/.shell/common ~/.shell/common.d/* \
-    ~/bin/**/* ~/.config/ranger/rc.conf ~/.config/awesome/rc.lua 2>/dev/null
-}
-
-pygrep() {
-    pattern=$1
-    a=$2
-    b=$3
-    [[ -z "$pattern" ]] && exit 1
-    [[ -z "$a" ]] && a=0
-    [[ -z "$b" ]] && b=0
-
-    # FIXME: Shouldn't hardcode these links in
-    # FIXME: This is not a bash-safe pattern
-    if [[ -n $ZSH_VERSION ]]; then
-        grep --color -Hn -P $pattern -A $a -B $b ~/kenjyco/(^env/)#*.py ~/extract_utils/(^env/)#*.py ~/redis_helper/(^env/)#*.py ~/chloop/(^env/)#*.py $(cat ~/.dotfiles_path)/bin/**/*.py 2>/dev/null
-    fi
+grepit() {
+    grep -iHn --color -R --exclude=\*.{pyc,swp} --exclude-dir=venv --exclude-dir=env --exclude-dir=.git $@ \.
 }
