292ec0d89c5a927b96f56b96abf8ccdd4c577224
Ken <kenjyco@gmail.com>
2016-05-03 21:52:11 -0500

    Update 'grepit' to not ignore case automatically

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -1,3 +1,3 @@
 grepit() {
-    grep -iHn --color -R --exclude=\*.{pyc,swp} --exclude-dir=venv --exclude-dir=env --exclude-dir=.git $@ \.
+    grep -Hn --color -R --exclude=\*.{pyc,swp} --exclude-dir=venv --exclude-dir=env --exclude-dir=.git $@ \.
 }
