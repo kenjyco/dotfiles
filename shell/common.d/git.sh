162f5edcf961361c9ea602aaa756a2336e1cf5b0
Ken <kenjyco@gmail.com>
2016-06-24 07:51:03 -0500

    Update 'glog' alias to ignore merge commits

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -1,6 +1,6 @@
 alias update-submodules="git submodule foreach --recursive git pull origin master"
 
-alias glog="git log --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an'"
+alias glog="git log --no-merges --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an'"
 alias glog2="glog --date local --name-status"
 
 alias gstatus="repo-status"
