055b0dc071508dfb48fc96f0be08b92d3af5d3b1
Ken <kenjyco@gmail.com>
2016-06-26 14:48:17 -0500

    Add 'common.d/xrandr.sh' with 'fix-monitors-*' shell functions

---
+++ shell/common.d/xrandr.sh
@@ -0,0 +1,16 @@
+fix-monitors-x200-below-1920() {
+    xrandr --output VGA1 --mode 1920x1080 --primary
+    xrandr --output LVDS1 --mode 1280x800 --below VGA1
+}
+
+fix-monitors-tv-vga() {
+    xrandr --output LVDS1 --off
+    xrandr --output VGA1 --mode 1920x1080
+    xrandr --output VGA1 --primary
+}
+
+fix-monitors-tv-hdmi() {
+    xrandr --output LVDS1 --off
+    xrandr --output HDMI1 --mode 1920x1080
+    xrandr --output HDMI1 --primary
+}
