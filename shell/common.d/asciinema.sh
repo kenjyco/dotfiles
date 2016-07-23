777ae74ab3d86bdc9e92866bbb29db0649ebdcf1
Ken <kenjyco@gmail.com>
2016-07-23 12:57:30 -0500

    Update 'asciicast' func to set title

--- shell/common.d/asciinema.sh
+++ shell/common.d/asciinema.sh
@@ -1,12 +1,14 @@
 asciicast() {
-    fname=$1
-    if [[ -z "$fname" ]]; then
-        fname="$(date +'%Y_%m%d-%a-%H%M%S').json"
+    title=$1
+    now_string="$(date +'%Y_%m%d-%a-%H%M%S')"
+    if [[ -z "$title" ]]; then
+        title="misc--${now_string}"
     else
-        fname="$fname--$(date +'%Y_%m%d-%a-%H%M%S').json"
+        title="${title}--${now_string}"
     fi
+    fname="$title.json"
 
     tmux -2 new-session -s cast -d
-    asciinema rec -c "tmux attach-session -t cast" -w 2 "$fname"
+    asciinema rec -c "tmux attach-session -t cast" -w 2 -t "$title" "$fname"
     echo -e "Saved recording to $fname"
 }
