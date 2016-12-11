23492e8be7add883803cf9544125586049df4328
Ken <kenjyco@gmail.com>
2016-12-10 21:38:11 -0600

    Add some shell funcs to 'common.d/beu.sh'

---
+++ shell/common.d/beu.sh
@@ -0,0 +1,15 @@
+# Don't set anything in this file if `~/.beu_path` does not exist
+[[ ! -f $HOME/.beu_path ]] && return 1
+BEU_PATH="$(cat $HOME/.beu_path)"
+
+beu() {
+    cd $BEU_PATH
+}
+
+beu-ipython() {
+    PYTHONPATH=$BEU_PATH $BEU_PATH/venv/bin/ipython $@
+}
+
+beu-test() {
+    PYTHONPATH=$BEU_PATH $BEU_PATH/venv/bin/py.test -vs $@ $BEU_PATH/tests
+}
