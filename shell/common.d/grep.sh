196a58cd6154fba533e994083c517c327350f378
Ken <kenjyco@gmail.com>
2017-01-30 10:01:14 -0600

    Add 'grepit-no-docs' shell func

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -1,3 +1,7 @@
 grepit() {
     grep -Hn --color -R --exclude=\*.{pyc,swp} --exclude-dir=venv --exclude-dir=env --exclude-dir=.git $@ \.
 }
+
+grepit-no-docs() {
+    grepit --exclude=\*.{txt,md,rst} $@
+}
