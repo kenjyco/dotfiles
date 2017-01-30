c4cd2573f1504a465357bde9a2f30834868d6e54
Ken <kenjyco@gmail.com>
2017-01-29 21:45:09 -0600

    Add 'fix-monitors-x200-portrait-right-1920' shell func to common.d/xrandr.sh

--- shell/common.d/xrandr.sh
+++ shell/common.d/xrandr.sh
@@ -3,6 +3,11 @@ fix-monitors-x200-below-1920() {
     xrandr --output LVDS1 --mode 1280x800 --below VGA1
 }
 
+fix-monitors-x200-portrait-right-1920() {
+    xrandr --output VGA1 --mode 1920x1080 --primary
+    xrandr --output LVDS1 --mode 1280x800 --rotate left --right-of VGA1
+}
+
 fix-monitors-tv-vga() {
     xrandr --output LVDS1 --off
     xrandr --output VGA1 --mode 1920x1080
