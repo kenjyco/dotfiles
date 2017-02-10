f49f77c1f7667ea0fa715f837823625fabea6b9d
Ken <kenjyco@gmail.com>
2017-02-10 10:44:13 -0600

    Use '-E' for grep in 'wallpapers/download.sh'

--- wallpapers/download.sh
+++ wallpapers/download.sh
@@ -9,5 +9,5 @@ fi
 
 grep "http" landscape.md |                              # Grab lines that have links
 perl -pe 's/^.*(http.*[^)]+)\)/$1/' |                   # Extract the links 
-grep -e '.*.(jpg|jpeg|png)$' |                          # Only keep image links
+grep -E '.*.(jpg|jpeg|png)$' |                          # Only keep image links
 xargs wget --no-clobber --directory-prefix=landscape    # Download the links with `wget`
