2e5a6f5871867618370e3695b160da7a190bb22f
Ken <kenjyco@gmail.com>
2017-03-31 09:06:33 -0500

    Add 'fix-monitors-t420-below-1920' shell func

--- shell/common.d/xrandr.sh
+++ shell/common.d/xrandr.sh
@@ -8,6 +8,11 @@ fix-monitors-t530-below-1920() {
     xrandr --output LVDS2 --mode 1920x1080 --below VGA2
 }
 
+fix-monitors-t420-below-1920() {
+    xrandr --output VGA1 --mode 1920x1080 --primary
+    xrandr --output LVDS1 --mode 1366x768 --below VGA1
+}
+
 fix-monitors-x200-portrait-right-1920() {
     xrandr --output VGA1 --mode 1920x1080 --primary
     xrandr --output LVDS1 --mode 1280x800 --rotate left --right-of VGA1
