a83adb388f39f295b3edeacbe2e9596bced1ea6f
Ken <kenjyco@gmail.com>
2017-04-01 14:04:15 -0500

    Remove the '--device' option from recordmydesktop call

--- shell/common.d/recordmydesktop.sh
+++ shell/common.d/recordmydesktop.sh
@@ -6,5 +6,5 @@ screencast() {
         fname="$fname--$(date +'%Y_%m%d-%a-%H%M%S').ogv"
     fi
 
-    recordmydesktop --on-the-fly-encoding --no-frame --full-shots --follow-mouse --fps 4 --device=hw:0,0 -o $fname
+    recordmydesktop --on-the-fly-encoding --no-frame --full-shots --follow-mouse --fps 4 -o $fname
 }
