043b2ca8093376da5d3068371d6e25e89653f17f
Ken <kenjyco@gmail.com>
2017-04-26 19:12:39 -0500

    Update 'screencast' shell func to have a better default filename and print shortcuts

--- shell/common.d/recordmydesktop.sh
+++ shell/common.d/recordmydesktop.sh
@@ -1,10 +1,11 @@
 screencast() {
     fname=$1
     if [[ -z "$fname" ]]; then
-        fname="$(date +'%Y_%m%d-%a-%H%M%S').ogv"
+        fname="screencast--$(date +'%Y_%m%d-%a-%H%M%S').ogv"
     else
         fname="$fname--$(date +'%Y_%m%d-%a-%H%M%S').ogv"
     fi
 
+    echo -e "\nctrl-alt-p to pause\nctrl-alt-s to stop\n"
     recordmydesktop --on-the-fly-encoding --no-frame --full-shots --follow-mouse --fps 4 -o $fname
 }
