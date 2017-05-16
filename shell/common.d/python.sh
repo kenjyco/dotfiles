bb5681c903b46d63e6a98974c0b9f69c8d800097
Ken <kenjyco@gmail.com>
2017-05-16 07:56:18 -0500

    Add 'vlc-helper' to make/update-home-venv (no longer provided by beu)

--- shell/common.d/python.sh
+++ shell/common.d/python.sh
@@ -76,14 +76,14 @@ make-home-venv() {
         install-home-venv-requirements
         cd
         python3 -m venv venv && venv/bin/pip3 install --upgrade pip wheel
-        venv/bin/pip3 install ipython flake8 grip jupyter beu
+        venv/bin/pip3 install ipython flake8 grip jupyter beu vlc-helper
     fi
 }
 
 update-home-venv() {
     [[ ! -d "$HOME/venv" ]] && echo "$HOME/venv does not exist" && return 1
     cd
-    venv/bin/pip3 install --upgrade ipython flake8 grip jupyter beu
+    venv/bin/pip3 install --upgrade ipython flake8 grip jupyter beu vlc-helper
 }
 
 home-ipython() {
