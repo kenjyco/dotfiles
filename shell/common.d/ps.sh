736da2be08b998a834c36faaca0339a054216cc5
Ken <kenjyco@gmail.com>
2017-05-16 07:41:46 -0500

    Add 'psome' alias to wrap 'p' alias (and filter out uninteresting processes)

--- shell/common.d/ps.sh
+++ shell/common.d/ps.sh
@@ -1,2 +1,3 @@
 alias p="ps -eo user,pid,ppid,tty,cmd:200 | grep -v ' \['"
 alias pa="ps -eo user,pid,ppid,tty,cmd:200"
+alias psome="p | egrep -v '(chromium|firefox|zsh|cinnamon|ssh-agent|kdeinit|dbus-launch|avahi-daemon|syndaemon|nemo|nm-applet|sshd|tmux|urxvt|\/usr\/|\/lib\/|\/sbin\/)'"
