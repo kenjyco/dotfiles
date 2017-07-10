dfb4a85514de63b193ca8bfa02d30f251b1e7b54
Ken <kenjyco@gmail.com>
2017-07-10 07:47:17 -0500

    Replace tab characters with spaces in 'common.d/scrot.sh'

--- shell/common.d/scrot.sh
+++ shell/common.d/scrot.sh
@@ -13,11 +13,11 @@ alias sc="screenshot-today"
 scloop() {
     message="<ENTER> take screenshot (select area with mouse), <CTRL>+<C> to stop loop "
     while true; do
-		if [[ -n "$BASH_VERSION" ]]; then
-			read -p "$message" yn
-		elif [[ -n "$ZSH_VERSION" ]]; then
-			vared -p "$message" -c yn
-		fi
+        if [[ -n "$BASH_VERSION" ]]; then
+            read -p "$message" yn
+        elif [[ -n "$ZSH_VERSION" ]]; then
+            vared -p "$message" -c yn
+        fi
         scrot -s '%Y_%m%d--%H%M_%S--'$(hostname)'--$wx$h.png'
     done
 }
@@ -25,11 +25,11 @@ scloop() {
 scloop-today() {
     message="<ENTER> take screenshot (select area with mouse), <CTRL>+<C> to stop loop "
     while true; do
-		if [[ -n "$BASH_VERSION" ]]; then
-			read -p "$message" yn
-		elif [[ -n "$ZSH_VERSION" ]]; then
-			vared -p "$message" -c yn
-		fi
+        if [[ -n "$BASH_VERSION" ]]; then
+            read -p "$message" yn
+        elif [[ -n "$ZSH_VERSION" ]]; then
+            vared -p "$message" -c yn
+        fi
         screenshot
     done
 }
