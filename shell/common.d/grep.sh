7fba3611b8ac671f171dd1a83523931f8ecb6fc2
Ken <kenjyco@gmail.com>
2017-10-08 14:14:36 -0500

    Add 'grepit-logs' shell func

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -2,6 +2,12 @@ grepit() {
     grep -Hn --color -R --exclude=\*.{pyc,swp,min.js} --exclude-dir=venv --exclude-dir=env --exclude-dir=node_modules --exclude-dir=.git $@ \.
 }
 
+grepit-logs() {
+    findit . --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
+        -o -path '*/build/*' -o -path '*/.git/*' -prune \) -iname '*.log'" \
+        --pipesort "grep -Hn --color $@"
+}
+
 grepit-no-docs() {
     grepit --exclude=\*.{txt,md,rst,log} --exclude-dir=\*.dist-info $@
 }
