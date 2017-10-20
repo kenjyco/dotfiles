7e39f25c6b64647fc42c1dcf32e3027cab00cc39
Ken <kenjyco@gmail.com>
2017-10-20 09:48:22 -0500

    Add 'grepit-exact', 'grep-history', and 'grep-history-exact' funcs

--- shell/common.d/grep.sh
+++ shell/common.d/grep.sh
@@ -13,9 +13,27 @@ grepit-no-docs() {
     grepit --exclude=\*.{txt,md,rst,log} --exclude-dir=\*.dist-info $@
 }
 
+grepit-exact() {
+    pattern=$1
+    [[ -z "$pattern" ]] && return 1
+    shift
+    grepit -p "\b$pattern\b" $@
+}
+
 grep-object-info() {
     object="$1"
     [[ -z "$object" ]] && return 1
     grepit-no-docs -p "\b$object\b" | egrep -o "($object\(|$object(\.\w+)+\(?)" |
     sort | uniq -c | sort -nr | egrep -v '.(js|py)$'
+}
+
+grep-history() {
+    grep -Hn --color $@ ~/.*history*
+}
+
+grep-history-exact() {
+    pattern=$1
+    [[ -z "$pattern" ]] && return 1
+    shift
+    grep-history -p "\b$pattern\b" $@
 }
