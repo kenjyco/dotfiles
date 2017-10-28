9d07c25dc7dac5d2f3e472327c597c2f6e970ab0
Ken <kenjyco@gmail.com>
2017-10-28 11:10:17 -0500

    Update findit wrappers to not determine dirname and to pass "$@" first

--- shell/common.d/findit.sh
+++ shell/common.d/findit.sh
@@ -1,259 +1,182 @@
 findit-swp() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --pattern ".*sw[po]" $@
+    findit "$@" --pattern ".*sw[po]"
 }
-alias swps="findit-swp . --stamp 2>/dev/null | sort"
+
+swps() {
+    findit-swp "$@" --stamp 2>/dev/null | sort
+}
 
 findit-py() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
-        -o -path '*/build/*' -prune \) -iname '*.py'" $@
+    findit "$@" --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
+        -o -path '*/build/*' -prune \) -iname '*.py'"
 }
 
 pys() {
-    findit-py --depth 1 --stamp $@ | sort
+    findit-py "$@" --depth 1 --stamp | sort
 }
 
 pys-all() {
-    findit-py --stamp $@ | sort
+    findit-py "$@" --stamp | sort
 }
 
 findit-js-backend() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-not \( -path '*/node_modules/*' -o -path '*/lib/*' \
+    findit "$@" --complex "-not \( -path '*/node_modules/*' -o -path '*/lib/*' \
         -o -path '*/static/*' -o -path '*/ui/*' -o -path '*/deploy/*' \
-        -o -path '*/test/*' -o -path '*/unitTests/*' -o -path '*/apidoc/*' -prune \) -iname '*.js'" $@
+        -o -path '*/test/*' -o -path '*/unitTests/*' -o -path '*/apidoc/*' -prune \) -iname '*.js'"
 }
 
 jss() {
-    findit-js-backend --depth 1 --stamp $@ | sort
+    findit-js-backend "$@" --depth 1 --stamp | sort
 }
 
 jss-all() {
-    findit-js-backend --stamp $@ | sort
+    findit-js-backend "$@" --stamp | sort
 }
 
 findit-autoday() {
-    findit ~/autoday $@
+    findit ~/autoday "$@"
 }
 
 findit-pics() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-not \( -path '*/.cache/*' -o -path '*/.config/*' \
+    findit "$@" --complex "-not \( -path '*/.cache/*' -o -path '*/.config/*' \
         -o -path '*/.thumbnails/*' -o -path '*/.cinnamon/*' \
         -o -path '*/venv/*' -o -path '*/env/*' -prune \) \
         \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \
-        -o -iname '*.gif' \)" $@
+        -o -iname '*.gif' \)"
 }
 
 pics() {
-    findit-pics --depth 1 --stamp $@ | sort
+    findit-pics "$@" --depth 1 --stamp | sort
 }
 
 pics-all() {
-    findit-pics --stamp $@ | sort
+    findit-pics "$@" --stamp | sort
 }
 
 pics-view() {
-    findit-pics --depth 1 $@ --pipesort "feh -Fd" &
+    findit-pics "$@" --depth 1 --pipesort "feh -Fd" &
 }
 
 pics-all-view() {
-    findit-pics $@ --pipesort "feh -Fd" &
+    findit-pics "$@" --pipesort "feh -Fd" &
 }
 
 findit-vids() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "\( -iname '*.mp4' -o -iname '*.flv' \
+    findit "$@" --complex "\( -iname '*.mp4' -o -iname '*.flv' \
         -o -iname '*.mkv' -o -iname '*.ogv' -o -iname '*.mov' \
-        -o -iname '*.webm' -o -iname '*.avi' \)" $@
+        -o -iname '*.webm' -o -iname '*.avi' \)"
 }
 
 vids() {
-    findit-vids --depth 1 --stamp $@ | sort
+    findit-vids "$@" --depth 1 --stamp | sort
 }
 
 vids-all() {
-    findit-vids --stamp $@ | sort
+    findit-vids "$@" --stamp | sort
 }
 
 vids-play() {
     _vlc="vlc"
     [[ $(uname) == 'Darwin' ]] && _vlc="/Applications/VLC.app/Contents/MacOS/VLC"
-    findit-vids --depth 1 $@ --pipesort "$_vlc --fullscreen"
+    findit-vids "$@" --depth 1 --pipesort "$_vlc --fullscreen"
 }
 
 vids-all-play() {
     _vlc="vlc"
     [[ $(uname) == 'Darwin' ]] && _vlc="/Applications/VLC.app/Contents/MacOS/VLC"
-    findit-vids $@ --pipesort "$_vlc --fullscreen" &>/dev/null &
+    findit-vids "$@" --pipesort "$_vlc --fullscreen" &>/dev/null &
 }
 
 findit-audio() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
+    findit "$@" --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
         -prune \) \( -iname '*.mp3' -o -iname '*.flac' \
-        -o -iname '*.ogg' -o -iname '*.wav' \)" $@
+        -o -iname '*.ogg' -o -iname '*.wav' \)"
 }
 
 audio() {
-    findit-audio --depth 1 --stamp $@ | sort
+    findit-audio "$@" --depth 1 --stamp | sort
 }
 
 audio-all() {
-    findit-audio --stamp $@ | sort
+    findit-audio "$@" --stamp | sort
 }
 
 audio-play() {
     mocp -S &>/dev/null
-    mocp -c && findit-audio --depth 1 $@ --pipesort "mocp -a" && mocp -p
+    mocp -c && findit-audio "$@" --depth 1 --pipesort "mocp -a" && mocp -p
 }
 
 audio-all-play() {
     mocp -S &>/dev/null
-    mocp -c && findit-audio $@ --pipesort "mocp -a" && mocp -p
+    mocp -c && findit-audio "$@" --pipesort "mocp -a" && mocp -p
 }
 
 findit-logs() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-iname '*.log' -type f ! -size 0" $@
+    findit "$@" --complex "-iname '*.log' -type f ! -size 0"
 }
 
 findit-logs-empty() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-iname '*.log' -type f -empty" $@
+    findit "$@" --complex "-iname '*.log' -type f -empty"
 }
 
 findit-logs-empty-delete() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-iname '*.log' -type f -empty -delete" $@
+    findit "$@" --complex "-iname '*.log' -type f -empty -delete"
 }
 
 logs() {
-    findit-logs $@ --depth 1 --stamp | sort
+    findit-logs "$@" --depth 1 --stamp | sort
 }
 
 logs-wcl() {
-    findit-logs $@ --depth 1 --pipesort 'wc -l'
+    findit-logs "$@" --depth 1 --pipesort 'wc -l'
 }
 
 logs-info() {
-    findit-logs $@ --depth 1 --pipesort 'grep -Hn --color INFO'
+    findit-logs "$@" --depth 1 --pipesort 'grep -Hn --color INFO'
 }
 
 logs-error() {
-    findit-logs $@ --depth 1 --pipesort 'grep -Hn --color ERROR'
+    findit-logs "$@" --depth 1 --pipesort 'grep -Hn --color ERROR'
 }
 
 logs-debug() {
-    findit-logs $@ --depth 1 --pipesort 'grep -Hn --color DEBUG'
+    findit-logs "$@" --depth 1 --pipesort 'grep -Hn --color DEBUG'
 }
 
 logs-all() {
-    findit-logs $@ --stamp | sort
+    findit-logs "$@" --stamp | sort
 }
 
 logs-all-wcl() {
-    findit-logs $@ --pipesort 'wc -l'
+    findit-logs "$@" --pipesort 'wc -l'
 }
 
 logs-all-info() {
-    findit-logs $@ --pipesort 'grep -Hn --color INFO'
+    findit-logs "$@" --pipesort 'grep -Hn --color INFO'
 }
 
 logs-all-error() {
-    findit-logs $@ --pipesort 'grep -Hn --color ERROR'
+    findit-logs "$@" --pipesort 'grep -Hn --color ERROR'
 }
 
 logs-all-debug() {
-    findit-logs $@ --pipesort 'grep -Hn --color DEBUG'
+    findit-logs "$@" --pipesort 'grep -Hn --color DEBUG'
 }
 
 findit-docs() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="."
-    elif [[ -z "$dirname" ]]; then
-        dirname="."
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
+    findit "$@" --complex "-not \( -path '*/venv/*' -o -path '*/env/*' \
         -prune \) \( -iname '*.pdf' -o -iname '*.doc' \
-        -o -iname '*.odt' -o -iname '*.md' \)" $@
+        -o -iname '*.odt' -o -iname '*.md' \)"
 }
 
 docs() {
-    findit-docs --depth 1 --stamp $@ | sort
+    findit-docs "$@" --depth 1 --stamp | sort
 }
 
 docs-all() {
-    findit-docs --stamp $@ | sort
+    findit-docs "$@" --stamp | sort
 }
 
 annotated() {
-    findit --pattern 'annotated*pdf' --stamp $@ | sort
+    findit "$@" --pattern 'annotated*pdf' --stamp | sort
 }
--- shell/common.d/idea.sh
+++ shell/common.d/idea.sh
@@ -1,13 +1,5 @@
 idea-list() {
-    dirname=$1
-    if [[ "$dirname" =~ '--.*' ]]; then
-        dirname="$HOME"
-    elif [[ -z "$dirname" ]]; then
-        dirname="$HOME"
-    else
-        shift 2>/dev/null
-    fi
-    findit $dirname --stamp --pattern "*.idea" $@ 2>/dev/null | sort
+    findit "$@" --stamp --pattern "*.idea" 2>/dev/null | sort
 }
 
 idea-rm-empty() {
