f16f9da4d8cff33197441b1b92a4aae634fdd0b9
Ken <kenjyco@gmail.com>
2017-11-04 09:30:23 -0500

    Update 'dfh' alias to work when using a Mac

--- shell/common.d/df.sh
+++ shell/common.d/df.sh
@@ -1,1 +1,5 @@
-alias dfh="df -Th --total | egrep -v '(^none|^udev|^tmpfs|^cgmfs)'"
+if [[ $(uname) == 'Darwin' ]]; then
+    alias dfh="df -lPh"
+else
+    alias dfh="df -Th --total | egrep -v '(^none|^udev|^tmpfs|^cgmfs)'"
+fi
