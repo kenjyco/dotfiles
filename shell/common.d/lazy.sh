c386dc630be64000867508698effe5d878547900
Ken <kenjyco@gmail.com>
2017-11-28 08:39:29 -0600

    Update lazy-update func to pass its args on to update-home-config

--- shell/common.d/lazy.sh
+++ shell/common.d/lazy.sh
@@ -1,5 +1,5 @@
 lazy-update() {
-    sshlazy && time (all-repos-update; update-home-config) || return
+    sshlazy && time (all-repos-update; update-home-config "$@") || return
     if [[ -n "$BASH_VERSION" ]]; then
         source ~/.bashrc
     elif [[ -n "$ZSH_VERSION" ]]; then
