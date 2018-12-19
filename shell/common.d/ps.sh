ab31231e101074a171625d0eacf6e52be093077a
Ken <kenjyco@gmail.com>
2018-12-19 16:45:48 -0600

    Update ps aliases to have decent output on mac

--- shell/common.d/ps.sh
+++ shell/common.d/ps.sh
@@ -1,3 +1,9 @@
-alias p="ps -eo user,pid,ppid,tty,cmd:200 | grep -v ' \['"
-alias pa="ps -eo user,pid,ppid,tty,cmd:200"
-alias psome="p | egrep -v '(chromium|firefox|zsh|cinnamon|ssh-agent|kdeinit|dbus-launch|avahi-daemon|syndaemon|nemo|nm-applet|sshd|tmux|urxvt|\/usr\/|\/lib\/|\/sbin\/)'"
+if [[ $(uname) == 'Darwin' ]]; then
+    alias p="ps aux | egrep -v '(^_|^root)'"
+    alias pa="ps aux"
+    alias psome="p | egrep -v '(ssh-agent|-bash$|/System/Library|/usr/libexec|/usr/sbin|SafeEjectGPUAgent|Google Chrome|/Applications/Slack.app|com\.docker\.)'"
+else
+    alias p="ps -eo user,pid,ppid,tty,cmd:200 | grep -v ' \['"
+    alias pa="ps -eo user,pid,ppid,tty,cmd:200"
+    alias psome="p | egrep -v '(chromium|firefox|zsh|cinnamon|ssh-agent|kdeinit|dbus-launch|avahi-daemon|syndaemon|nemo|nm-applet|sshd|tmux|urxvt|\/usr\/|\/lib\/|\/sbin\/)'"
+fi
