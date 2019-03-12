ae339df638b865c5c6d785a43dc5410b388555b1
Ken <kenjyco@gmail.com>
2019-03-12 08:24:14 -0500

    Update redis-usage to know default dump.rdb paths in mac vs debian
    Also update how rdb is invoked based on operating system

--- shell/common.d/redis.sh
+++ shell/common.d/redis.sh
@@ -1,6 +1,12 @@
 redis-usage() {
     dump_path=$1
-    [[ -z "$dump_path" ]] && dump_path="/var/lib/redis/dump.rdb"
+    if [[ -z "$dump_path" ]]; then
+        if [[ -d /var/lib/redis ]]; then
+            dump_path="/var/lib/redis/dump.rdb"
+        elif [[ -d /usr/local/var/db/redis ]]; then
+            dump_path="/usr/local/var/db/redis/dump.rdb"
+        fi
+    fi
     seconds_since_save=$(($(date +"%s") - $(redis-cli lastsave)))
     if [[ $seconds_since_save -gt 1000 ]]; then
         redis-cli bgsave >/dev/null
@@ -11,9 +17,9 @@ redis-usage() {
         done
         echo "Finished bgsave command" >&2
     fi
-    if [[ ! -f "$dump_path" ]]; then
-        echo "Dump file $dump_path was not found. Call again and pass in path to the dump.rdb file" >&2
-        return 1
+    if [[ $(uname) == 'Darwin' ]]; then
+        rdb -c memory "$dump_path" | sort -t, -k4 -h
+    else
+        sudo rdb -c memory "$dump_path" | sort -t, -k4 -h
     fi
-    rdb -c memory "$dump_path" | sort -t, -k4 -h
 }
