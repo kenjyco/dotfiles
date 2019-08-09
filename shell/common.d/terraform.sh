0f4f1c2d6aa4e1827f09e4e17a4122d96c07dbf9
Ken <kenjyco@gmail.com>
2019-08-09 18:24:21 -0500

    Add get-terraform shell func and update .gitignore

---
+++ shell/common.d/terraform.sh
@@ -0,0 +1,14 @@
+get-terraform() {
+    if [[ ! -f "$HOME/bin/terraform" ]]; then
+        oldpwd=$(pwd)
+        cd /tmp
+        if [[ $(uname) == 'Darwin' ]]; then
+            wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_darwin_amd64.zip
+            unzip terraform_0.11.8_darwin_amd64.zip -d "$HOME/bin"
+        else
+            wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
+            unzip terraform_0.11.8_linux_amd64.zip -d "$HOME/bin"
+        fi
+        cd "$oldpwd"
+    fi
+}
