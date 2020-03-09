e3f65d9f778e62b090ec3d5f5289648fb2797c8d
Ken <kenjyco@gmail.com>
2020-03-09 01:18:27 -0500

    Remove MANY scripts from bin/git that are no longer needed
    Also update `prune` to not call repo-remote and remove
    gstatus/gstatus-all aliases (from common.d/git.sh)

--- shell/common.d/git.sh
+++ shell/common.d/git.sh
@@ -3,6 +3,3 @@ alias update-submodules="git submodule foreach --recursive git pull origin maste
 alias glog="git log --find-renames --no-merges --pretty=format:'%C(yellow)%h %C(reset)%s %C(red)%ad %C(blue)%an%C(reset)'"
 alias glog2="glog --date local --name-status"
 alias glog3="git log --find-renames --stat --reverse -p"
-
-alias gstatus="repo-status--changes-only"
-alias gstatus-all="all-repos-status--changes-only"
